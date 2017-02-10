17e4f116172fd55b9ec6987ca1ee7a654bc9f492
Byeonghak Ko <quark2930@hotmail.com>
2017-02-11 02:44:10 +0900

    I forgot adding universaldrawer.py in the previous commit

--- MuonAnalyser/test/universaldrawer.py
+++ MuonAnalyser/test/universaldrawer.py
@@ -6,9 +6,6 @@ ROOT.gROOT.SetBatch(True)
 tdrstyle.setTDRStyle()
 
 
-hA = ""
-
-
 def setMarkerStyle(h,color,style):
     h.SetMarkerColor(color)
     h.SetMarkerStyle(style)
@@ -33,7 +30,6 @@ def getEff(filename,treename,title,binning,plotvar,dencut,numcut):
     h2 = makeTH1(filename,treename,title,binning,plotvar,numcut)
     hEff = ROOT.TEfficiency(h2,h1)
     hEff.SetTitle(title)
-    hA = copy.deepcopy(h1)
     return copy.deepcopy(hEff)
 
 
@@ -42,7 +38,7 @@ def drawSampleName(samplename):
     tex2.SetNDC()
     tex2.SetTextFont(42)
     tex2.SetTextSize(0.04)
-    tex2.DrawLatex(0.18, 0.8, samplename)
+    tex2.DrawLatex(0.18, 0.85, samplename)
 
 
 datadir = os.environ["CMSSW_BASE"]+'/src/MuonPerformance/MuonAnalyser/test/'
@@ -55,36 +51,61 @@ if isotype not in listIso:
     print "Error : Isolation condition (second arg) should be Trk or PF"
     exit(1)
 
+fMaxPuppi = 0.5
+nNumBinPuppi = 20
+
 arrPlotvar = [
-    {"plotvar": "genMuon.Pt()",        "binning": [10,5,105]}, 
-    {"plotvar": "abs(genMuon.Eta())",  "binning": [15,0,2.4]}, 
-    #{"plotvar": "genMuon.Phi()",       "binning": [12,-3,3]}, 
+    {"plotvar": "genMuon.Pt()",        "binning": [10,5,105], 
+        "xtitle": "p_{T} (GeV)", "ytitle": "Efficiency", "min": 0.6, "max": 1.1}, 
+    {"plotvar": "abs(genMuon.Eta())",  "binning": [15,0,2.4],
+        "xtitle": "|#eta|",      "ytitle": "Efficiency", "min": 0.6, "max": 1.1}, 
+    #{"plotvar": "genMuon.Phi()",       "binning": [12,-3,3],
+    #    "xtitle": "#phi",        "ytitle": "Efficiency", "min": 0.6, "max": 1.1}, 
     
-    {"plotvar": "recoMuon.Pt()",       "binning": [10,5,105]}, 
-    {"plotvar": "abs(recoMuon.Eta())", "binning": [8,0,2.4]}, 
-    #{"plotvar": "recoMuon.Phi()",      "binning": [12,-3,3]}, 
+    {"plotvar": "recoMuon.Pt()",       "binning": [10,5,105],
+        "xtitle": "p_{T} (GeV)", "ytitle": "Fake Rate", "max": 1.2}, 
+    {"plotvar": "abs(recoMuon.Eta())", "binning": [8,0,2.4],
+        "xtitle": "|#eta|",      "ytitle": "Fake Rate", "max": 1.2}, 
+    #{"plotvar": "recoMuon.Phi()",      "binning": [12,-3,3],
+    #    "xtitle": "#phi",        "ytitle": "Fake Rate", "max": 1.2}, 
     
-    {"plotvar": "recoMuon_TrkIsolation03", "binning": [20,0,1], "id": "Tight", "ylog": True, "type": "all"}, 
-    {"plotvar": "recoMuon_PFIsolation04",  "binning": [20,0,1], "id": "Tight", "ylog": True, "type": "all"}, 
+    {"plotvar": "recoMuon_TrkIsolation03", "binning": [20,0,1], "id": "Tight", "type": "all", 
+        "ylog": True, "xtitle": "Track Isolation", "ytitle": "# of events (normalized)"}, 
+    {"plotvar": "recoMuon_PFIsolation04",  "binning": [20,0,1], "id": "Tight", "type": "all",
+        "ylog": True, "xtitle": "PF Isolation", "ytitle": "# of events (normalized)"}, 
     
-    {"plotvar": "recoMuon_TrkIsolation03", "binning": [20,0,1], "id": "Loose", "ylog": True, "type": "all"}, 
-    {"plotvar": "recoMuon_PFIsolation04",  "binning": [20,0,1], "id": "Loose", "ylog": True, "type": "all"}, 
+    {"plotvar": "recoMuon_TrkIsolation03", "binning": [20,0,1], "id": "Loose", "type": "all",
+        "ylog": True, "xtitle": "Track Isolation", "ytitle": "# of events (normalized)"}, 
+    {"plotvar": "recoMuon_PFIsolation04",  "binning": [20,0,1], "id": "Loose", "type": "all",
+        "ylog": True, "xtitle": "PF Isolation", "ytitle": "# of events (normalized)"}, 
     
-    {"plotvar": "genMuon_puppiIsoWithLep",     "binning": [20,0,1], "id": "Tight", "ylog": True, "type": "all"}, 
-    {"plotvar": "genMuon_puppiIsoWithoutLep",  "binning": [20,0,1], "id": "Tight", "ylog": True, "type": "all"}, 
-    {"plotvar": "genMuon_puppiIsoCombined",    "binning": [20,0,1], "id": "Tight", "ylog": True, "type": "all"}, 
+    {"plotvar": "genMuon_puppiIsoWithLep",     "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Tight", "type": "all",
+        "ylog": True, "xtitle": "PUPPI Isolation with lepton", "ytitle": "# of events (normalized)"}, 
+    {"plotvar": "genMuon_puppiIsoWithoutLep",  "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Tight", "type": "all",
+        "ylog": True, "xtitle": "PUPPI Isolation without lepton", "ytitle": "# of events (normalized)"}, 
+    {"plotvar": "genMuon_puppiIsoCombined",    "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Tight", "type": "all",
+        "ylog": True, "xtitle": "Combined PUPPI Isolation (ratio = 0.5)", "ytitle": "# of events (normalized)"}, 
     
-    {"plotvar": "genMuon_puppiIsoWithLep",     "binning": [20,0,1], "id": "Loose", "ylog": True, "type": "all"}, 
-    {"plotvar": "genMuon_puppiIsoWithoutLep",  "binning": [20,0,1], "id": "Loose", "ylog": True, "type": "all"}, 
-    {"plotvar": "genMuon_puppiIsoCombined",    "binning": [20,0,1], "id": "Loose", "ylog": True, "type": "all"}, 
+    {"plotvar": "genMuon_puppiIsoWithLep",     "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Loose", "type": "all",
+        "ylog": True, "xtitle": "PUPPI Isolation with lepton", "ytitle": "# of events (normalized)"}, 
+    {"plotvar": "genMuon_puppiIsoWithoutLep",  "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Loose", "type": "all",
+        "ylog": True, "xtitle": "PUPPI Isolation without lepton", "ytitle": "# of events (normalized)"}, 
+    {"plotvar": "genMuon_puppiIsoCombined",    "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Loose", "type": "all",
+        "ylog": True, "xtitle": "Combined PUPPI Isolation (ratio = 0.5)", "ytitle": "# of events (normalized)"}, 
     
-    {"plotvar": "recoMuon_puppiIsoWithLep",    "binning": [20,0,1], "id": "Tight", "ylog": True, "type": "all"}, 
-    {"plotvar": "recoMuon_puppiIsoWithoutLep", "binning": [20,0,1], "id": "Tight", "ylog": True, "type": "all"}, 
-    {"plotvar": "recoMuon_puppiIsoCombined",   "binning": [20,0,1], "id": "Tight", "ylog": True, "type": "all"}, 
+    {"plotvar": "recoMuon_puppiIsoWithLep",    "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Tight", "type": "all",
+        "ylog": True, "xtitle": "PUPPI Isolation with lepton", "ytitle": "# of events (normalized)"}, 
+    {"plotvar": "recoMuon_puppiIsoWithoutLep", "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Tight", "type": "all",
+        "ylog": True, "xtitle": "PUPPI Isolation without lepton", "ytitle": "# of events (normalized)"}, 
+    {"plotvar": "recoMuon_puppiIsoCombined",   "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Tight", "type": "all",
+        "ylog": True, "xtitle": "Combined PUPPI Isolation (ratio = 0.5)", "ytitle": "# of events (normalized)"}, 
     
-    {"plotvar": "recoMuon_puppiIsoWithLep",    "binning": [20,0,1], "id": "Loose", "ylog": True, "type": "all"}, 
-    {"plotvar": "recoMuon_puppiIsoWithoutLep", "binning": [20,0,1], "id": "Loose", "ylog": True, "type": "all"}, 
-    {"plotvar": "recoMuon_puppiIsoCombined",   "binning": [20,0,1], "id": "Loose", "ylog": True, "type": "all"}, 
+    {"plotvar": "recoMuon_puppiIsoWithLep",    "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Loose", "type": "all",
+        "ylog": True, "xtitle": "PUPPI Isolation with lepton", "ytitle": "# of events (normalized)"}, 
+    {"plotvar": "recoMuon_puppiIsoWithoutLep", "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Loose", "type": "all",
+        "ylog": True, "xtitle": "PUPPI Isolation without lepton", "ytitle": "# of events (normalized)"}, 
+    {"plotvar": "recoMuon_puppiIsoCombined",   "binning": [nNumBinPuppi,0,fMaxPuppi], "id": "Loose", "type": "all",
+        "ylog": True, "xtitle": "Combined PUPPI Isolation (ratio = 0.5)", "ytitle": "# of events (normalized)"}, 
 ]
 
 """
@@ -100,13 +121,19 @@ arrSampleType = [
 """
 
 arrSampleType = [
-    {"title": "Phase II PU0",   "filename": "puppi_PU0.root",   "id": "Tight", "color": 4, "shape": 20}, # blue,  filled circle
-    #{"title": "Phase II PU200", "filename": "puppi_PU200.root", "id": "Tight", "color": 2, "shape": 21}, # red,   filled square
-    {"title": "Phase II QCD",   "filename": "puppi_QCD.root",   "id": "Tight", "color": 1, "shape": 34}, # black, filled cross
+    {"title": "Phase II PU0",   "filename": "puppi_PU0.root",   "id": "Tight", 
+        "color": 4, "shape": 20}, # blue,  filled circle
+    #{"title": "Phase II PU200", "filename": "puppi_PU200.root", "id": "Tight", 
+    #    "color": 2, "shape": 21}, # red,   filled square
+    {"title": "Phase II QCD",   "filename": "puppi_QCD.root",   "id": "Tight",
+        "color": 1, "shape": 34}, # black, filled cross
     
-    {"title": "Phase II PU0",   "filename": "puppi_PU0.root",   "id": "Loose", "color": 3, "shape": 24}, # green, unfilled circle
-    #{"title": "Phase II PU200", "filename": "puppi_PU200.root", "id": "Loose", "color": 6, "shape": 25}, # pink,  unfilled square
-    {"title": "Phase II QCD",   "filename": "puppi_QCD.root",   "id": "Loose", "color": 7, "shape": 28}, # cyan,  unfilled cross
+    {"title": "Phase II PU0",   "filename": "puppi_PU0.root",   "id": "Loose",
+        "color": 3, "shape": 24}, # green, unfilled circle
+    #{"title": "Phase II PU200", "filename": "puppi_PU200.root", "id": "Loose", 
+    #    "color": 6, "shape": 25}, # pink,  unfilled square
+    {"title": "Phase II QCD",   "filename": "puppi_QCD.root",   "id": "Loose",
+        "color": 7, "shape": 28}, # cyan,  unfilled cross
 ];
 
 listIDCfg = {
@@ -114,13 +141,14 @@ listIDCfg = {
     "Loose": {"isoCut": {"Trk": "TrkIsolation03 < 0.10", "PF": "PFIsolation04 < 0.25"}}, 
 }
 
-strCutGenNor = "genMuon.Pt() > 5"
+strCutGenNor = "genMuon.Pt() > 5 && abs(genMuon.Eta()) < 2.4"
 strCutGenDen = strCutGenNor
 #strCutGenNum = strCutGenDen + " && genMuon_is%(ID)s && genMuon_%(iso)s" # ID is held
 strCutGenNum = strCutGenDen + " && genMuon_isMuon && genMuon_%(iso)s"
 
-strCutRecNor = "recoMuon.Pt() > 5"
-#strCutRecDen = strCutRecNor + " && recoMuon_is%(ID)s && recoMuon_%(iso)s" # ID is held
+#strCutRecNor = "recoMuon.Pt() > 5 && recoMuon_is%(ID)s"
+#strCutRecNor = "recoMuon.Pt() > 5"
+strCutRecNor = "recoMuon.Pt() > 5 && abs(recoMuon.Eta()) < 2.4"
 strCutRecDen = strCutRecNor + " && recoMuon_isMuon && recoMuon_%(iso)s"
 strCutRecNum = strCutRecDen + " && !recoMuon_signal"
 
@@ -166,7 +194,7 @@ for i, dicPlotvar in enumerate(arrPlotvar):
         # This is the isolation cut condition
         strCutIso = listIDCfg[ strIDCurr ][ "isoCut" ][ isotype ]
         
-        if "Isolation" not in plotvar: 
+        if "Iso" not in plotvar: 
             # Drawing efficiency / fake rate plot
             sampHead[ "hist" ] = getEff(datadir + sampHead[ "filename" ], strTree, 
                 sampHead[ "title" ] + " - " + strIDCurr, binCurr, plotvar, 
@@ -188,36 +216,25 @@ for i, dicPlotvar in enumerate(arrPlotvar):
     h_init = ROOT.TH1F("", "", binCurr[ 0 ], binCurr[ 1 ], binCurr[ 2 ])
 
     #Set axis
-    x_name = "Muon "
-    if "Pt"  in plotvar: x_name = x_name+"p_{T}"
-    if "Eta" in plotvar: x_name = x_name+"|#eta|"
-    if "Phi" in plotvar: x_name = x_name+"#phi"
-
-    y_name = "Muon "
+    x_name = "Muon " + dicPlotvar[ "xtitle" ]
+    y_name = "Muon " + dicPlotvar[ "ytitle" ]
     
-    if "genMuon" in plotvar:
-        h_init.SetMaximum(1.1)
-        h_init.SetMinimum(0.6)
-        y_name = y_name+"Efficiency"
-    
-    if "recoMuon" in plotvar:
-        #h_init.SetMaximum(max(h.GetMaximum() for h in hlist)*2.5)
-        h_init.GetYaxis().SetLabelSize(0.035)
-        h_init.GetYaxis().SetTitleOffset(1.2)
-        y_name = y_name+"Fake Rate"
+    if "min" in dicPlotvar: h_init.SetMinimum(dicPlotvar[ "min" ])
+    if "max" in dicPlotvar: h_init.SetMaximum(dicPlotvar[ "max" ])
     
     h_init.GetXaxis().SetTitle(x_name)
     h_init.GetYaxis().SetTitle(y_name)
     h_init.GetYaxis().SetTitleOffset(1)
 
     name = "%s_%s"%(plotvar, isotype)
+    if "id" in dicPlotvar: name = name + "_%s"%dicPlotvar[ "id" ]
 
     #Set canvas
     canv = makeCanvas(name, False)
     setMargins(canv, False)
     h_init.Draw()
     #drawSampleName("Z/#gamma^{*}#rightarrow#font[12]{#mu#mu}, p_{T} > 5 GeV")
-    drawSampleName("10 #mu per event and QCD events, p_{T} > 5 GeV")
+    drawSampleName("Z/#gamma^{*}#rightarrow#font[12]{#mu#mu} and QCD events, p_{T} > 5 GeV, |#eta| < 2.4")
     
     if "ylog" in dicPlotvar and dicPlotvar[ "ylog" ]:
         ROOT.gPad.SetLogy()
@@ -225,7 +242,7 @@ for i, dicPlotvar in enumerate(arrPlotvar):
         ROOT.gPad.SetLogy(0)
 
     #Legend and drawing
-    legTop = ROOT.TLegend(0.6,0.7,0.85,0.85)
+    legTop = ROOT.TLegend(0.6,0.65,0.85,0.80)
     legBot = ROOT.TLegend(0.6,0.2,0.85,0.35)
     if "genMuon"  in plotvar: leg = legBot
     if "recoMuon" in plotvar: leg = legTop
