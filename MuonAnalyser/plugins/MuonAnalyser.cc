bbd1a604148a75814fc463f8bcc1be10dc6cf311
Jason Lee <jason.lee@cern.ch>
2017-08-24 19:18:59 +0900

    fix for pre3

--- MuonAnalyser/plugins/MuonAnalyser.cc
+++ MuonAnalyser/plugins/MuonAnalyser.cc
@@ -203,6 +203,7 @@ MuonAnalyser::MuonAnalyser(const edm::ParameterSet& pset)
   
   ParameterSet tpset = pset.getParameter<ParameterSet>("tpSelector");
   tpSelector_ = TrackingParticleSelector(tpset.getParameter<double>("ptMin"),
+					 tpset.getParameter<double>("ptMax"),
                                          tpset.getParameter<double>("minRapidity"),
                                          tpset.getParameter<double>("maxRapidity"),
                                          tpset.getParameter<double>("tip"),
