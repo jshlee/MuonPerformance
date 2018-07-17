e31eddb94ad3a1b7c31901f5b196d5807cd6234d
Ian J. Watson <ian.james.watson@cern.ch>
2018-07-17 12:49:22 +0900

    Some updates

--- MuonAnalyser/test/runSliceTestAnalysis.py
+++ MuonAnalyser/test/runSliceTestAnalysis.py
@@ -11,18 +11,20 @@ process.load('RecoMuon.TrackingTools.MuonServiceProxy_cff')
 process.load('TrackingTools.TransientTrack.TransientTrackBuilder_cfi')
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, '101X_dataRun2_Prompt_v10', '')
-#process.MessageLogger.cerr.FwkReport.reportEvery = 5000
+process.MessageLogger.cerr.FwkReport.reportEvery = 5000
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
-process.maxEvents.input = cms.untracked.int32(10)
+#process.maxEvents.input = cms.untracked.int32(10)
 # Input source
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
-process.source.skipEvents = cms.untracked.uint32(17601)
+process.source.skipEvents = cms.untracked.uint32(0)
 
 #process.source.fileNames.append('/store/data/Run2018B/Cosmics/AOD/PromptReco-v1/000/317/428/00000/E4BC1D7B-3F6A-E811-9E05-FA163E57A064.root')
-process.source.fileNames.append('/store/user/jlee/SingleMuon/Run2017G-v1/FEVTEvent/step3_313.root')
+from glob import glob
+process.source.fileNames.extend(['file:'+f for f in glob('/xrootd/store/user/jlee/SingleMuon/Run2017G-v1/RECOv2/step3*.root')][:5])
+#process.source.fileNames.append('/store/user/jlee/SingleMuon/Run2017G-v1/FEVTEvent/step3_313.root')
 
 #fname = 'singleMuon.txt'
 #f = open(fname)
